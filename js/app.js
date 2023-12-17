816433cee88eaf21c19264999c6fe7a8fa909631
ts <ts.titang@gmail.com>
2023-12-18 03:34:47 +0700

    limit

--- js/app.js
+++ js/app.js
@@ -56,6 +56,12 @@ class CalorieTracker {
 		this._render();
 	}
 
+	setLimit(calorieLimit) {
+		this._calorieLimit = calorieLimit;
+		this._displayCaloriesLimit();
+		this._render();
+	}
+
 	///Private methods
 
 	_displayCaloriesTotal() {
@@ -217,6 +223,10 @@ class App {
 		document
 			.getElementById('reset')
 			.addEventListener('click', this._reset.bind(this));
+
+		document
+			.getElementById('limit-form')
+			.addEventListener('submit', this._setLimit.bind(this));
 	}
 
 	_newItem(type, e) {
@@ -284,6 +294,24 @@ class App {
 		document.getElementById('workout-items').innerHTML = '';
 		document.getElementById('filter-meals').value = '';
 		document.getElementById('filter-workouts').value = '';
+	}
+
+	_setLimit(e) {
+		e.preventDefault();
+
+		const limit = document.getElementById('limit');
+
+		if (limit.value === '') {
+			alert('Please fill in limit field');
+			return;
+		}
+
+		this._tracker.setLimit(+limit.value);
+		limit.value = '';
+
+		const modalEl = document.getElementById('limit-modal');
+		const modal = bootstrap.Modal.getInstance(modalEl);
+		modal.hide();
 	}
 }
 

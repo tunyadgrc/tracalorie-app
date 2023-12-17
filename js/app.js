3aab9af920a638a5d1b02d472b168e6da4dfc48c
ts <ts.titang@gmail.com>
2023-12-18 03:16:00 +0700

    remove meal/workout

--- js/app.js
+++ js/app.js
@@ -22,11 +22,31 @@ class CalorieTracker {
 		this._render();
 	}
 
+	removeMeal(id) {
+		const index = this._meals.findIndex((meal) => meal.id === id);
+		if (index !== -1) {
+			const meal = this._meals[index];
+			this._totalCalories -= meal.calories;
+			this._meals.splice(index, 1);
+			this._render();
+		}
+	}
+
 	addWorkout(workout) {
 		this._workouts.push(workout);
 		this._totalCalories -= workout.calories;
 		this._displayNewWorkout(workout);
 		this._render();
+	}
+
+	removeWorkout(id) {
+		const index = this._workouts.findIndex((workout) => workout.id === id);
+		if (index !== -1) {
+			const workout = this._workouts[index];
+			this._totalCalories += workout.calories;
+			this._workouts.splice(index, 1);
+			this._render();
+		}
 	}
 
 	///Private methods
@@ -170,6 +190,14 @@ class App {
 		document
 			.getElementById('workout-form')
 			.addEventListener('submit', this._newItem.bind(this, 'workout'));
+
+		document
+			.getElementById('meal-items')
+			.addEventListener('click', this._removeItem.bind(this, 'meal'));
+
+		document
+			.getElementById('workout-items')
+			.addEventListener('click', this._removeItem.bind(this, 'workout'));
 	}
 
 	_newItem(type, e) {
@@ -199,6 +227,24 @@ class App {
 		const bsCollapse = new bootstrap.Collapse(collapseItem, {
 			toggle: true,
 		});
+	}
+
+	_removeItem(typr, e) {
+		if (
+			e.target.classList.contains(
+				'delete' || e.target.classList.contains('fa-xmark')
+			)
+		) {
+			if (confirm('Are you sure?')) {
+				const id = e.target.closest('.card').getAttribute('data-id');
+
+				type === 'meal'
+					? this._tracker.removeMeal(id)
+					: this._tracker.removeWorkout(id);
+
+				e.target.closest('.card').remove();
+			}
+		}
 	}
 }
 
